1d925d4f89a948d8897e97dcb4dc38901ff081bb
Rodrigo - Contactus <rodrigo.loureiro.gomes@gmail.com>
2026-03-18 17:12:10 +0000

    fix: credenciais estáticas de login, alterações estéticas

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -16,10 +16,10 @@ export class LoginComponent {
   password = '';
 
   login() {
-    if (this.username === 'admin' && this.password === 'password') {
+    if (this.username.trim() === 'admin' && this.password.trim() === 'password') {
       this.auth.login('admin');
       this.router.navigate(['/admin']);
-    } else if (this.username === 'user' && this.password === 'password') {
+    } else if (this.username.trim() === 'user' && this.password.trim() === 'password') {
       this.auth.login('user');
       this.router.navigate(['/tarefas-abertas']);
     } else {
